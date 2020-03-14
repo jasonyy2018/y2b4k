1067ec58fb03369b6bd00bf3e91fdb415c94d542
jason <290899973@qq.com>
2020-03-14 10:33:37 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 define('APIKEY', 'AIzaSyB5zY2fq6YRSMxF7HwGb8rEQ8VVRxiO6ps');
-define('GJ_CODE', 'HK');
-define('SITE_NAME', 'you2b1eclub');
-define('TITLENAME', 'you2beclub');
+define('GJ_CODE', 'US');
+define('SITE_NAME', 'utuby');
+define('TITLENAME', 'utuby');
 define('EN2DEKEY', '32123wwerwertgfdgds');
-define('EMAIL', '1334347212@qq.com');
+define('EMAIL', 'jasonyu.jy2016@gmail.com');
 define('NAME', 'admin');
 define('PASSWORD', 'admin');
 define('LOGINHTML', '<!DOCTYPE html>
